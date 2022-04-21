08175720c2b2e2c961fa68224b341a39387134cb
James Graham <james@hoppipolla.co.uk>
2022-04-21 11:50:55 +0100

    Switch to using Rust CDDL implementation.
    This has a couple of advantages:
    * It prints a more useful error message when it fails, so it's easier
    to identify the point of failure
    * It supports validation, so we can write tests against the CDDL
    defined in the spec.
    * It seems to be more actively maintained

---
+++ scripts/test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -ex
+
+SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
+ROOT=$(dirname $SCRIPT_DIR)
+
+if [ "$1" = "--install" ]; then
+    cargo install cddl
+    npm install parse5
+fi
+$ROOT/scripts/cddl/generate.js
+
+cddl compile-cddl --cddl local.cddl
+cddl compile-cddl --cddl remote.cddl
