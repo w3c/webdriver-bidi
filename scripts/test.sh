bb4b6385a112bdb80561f5388ec27826368cb978
Henrik Skupin <mail@hskupin.info>
2022-09-13 20:50:57 +0200

    Update scripts to only install dependencies if not yet installed or requested

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,16 @@ set -ex
 SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
 ROOT=$(dirname $SCRIPT_DIR)
 
-if [ "$1" = "--install" ]; then
-    cargo install cddl
-    npm install parse5
+if ! [ -x "$(command -v cddl)" ] || [ "$1" = "--upgrade" ]; then
+  echo 'Installing cddl'
+  cargo install cddl
 fi
+
+if [[ "$(npm list parse5)" =~ "empty" ]] || [ "$1" = "--upgrade" ]; then
+  echo 'Installing npm package parse5'
+  npm install parse5
+fi
+
 # Extract CDDL content from spec into files
 $ROOT/scripts/cddl/generate.js
 
