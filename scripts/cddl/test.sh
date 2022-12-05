ed7ebf98b1ebf01edf079c1eb4b84389a9db5b8f
Maksim Sadym <sadym@google.com>
2022-12-05 19:48:08 +0100

    test.sh tests both CDDL and no splits

---
+++ scripts/cddl/test.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+set -ex
+
+SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
+ROOT=$(dirname $SCRIPT_DIR)
+
+if ! [ -x "$(command -v cddl)" ] || [ "$1" = "--upgrade" ]; then
+  echo 'Installing cddl'
+  cargo install cddl
+fi
+
+if [[ "$(npm list parse5)" =~ "empty" ]] || [ "$1" = "--upgrade" ]; then
+  echo 'Installing npm package parse5'
+  npm install parse5
+fi
+
+# Extract CDDL content from spec into files
+$ROOT/cddl/generate.js
+
+cddl compile-cddl --cddl local.cddl
+cddl compile-cddl --cddl remote.cddl
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,18 +4,5 @@ set -ex
 SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
 ROOT=$(dirname $SCRIPT_DIR)
 
-if ! [ -x "$(command -v cddl)" ] || [ "$1" = "--upgrade" ]; then
-  echo 'Installing cddl'
-  cargo install cddl
-fi
-
-if [[ "$(npm list parse5)" =~ "empty" ]] || [ "$1" = "--upgrade" ]; then
-  echo 'Installing npm package parse5'
-  npm install parse5
-fi
-
-# Extract CDDL content from spec into files
-$ROOT/scripts/cddl/generate.js
-
-cddl compile-cddl --cddl local.cddl
-cddl compile-cddl --cddl remote.cddl
+sh $ROOT/scripts/cddl/test.sh
+node $ROOT/scripts/formatter/no_split_var.js
